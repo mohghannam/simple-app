9f55f935ba1c51a4b7cbd7c02368c72a304f766d
mohghannam <ayman@new.openshift.linuxplus.local>
2022-04-10 19:56:47 +0200

    update port number

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html", host_name=socket.gethostname())
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='8080')
