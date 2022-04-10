5c360061bb54631da1a6831a8f524dbc1cca91b7
root <root@new.openshift.linuxplus.local>
2022-04-10 11:07:47 +0200

    first push

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+import socket
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html", host_name=socket.gethostname())
+if __name__ == '__main__':
+    app.run(debug=True)
